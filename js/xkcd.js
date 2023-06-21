830f36421b5d9e560f902aed2e7385e53484245a
creepydanunity <mr.creepydanx@gmail.com>
2023-06-22 00:33:28 +0300

    XKCD comics added

---
+++ js/xkcd.js
@@ -0,0 +1,25 @@
+function getXKCD() {
+    const paramsString = new URLSearchParams({ email: 'd.vasilev@innopolis.university' });
+    const url = `https://fwd.innopolis.app/api/hw2/?${paramsString}`;
+
+
+    fetch(url)
+   .then(response => response.text())
+   .then((response) => {
+        console.log(response);
+        const paramString = new URLSearchParams({ id: response});
+        fetch(`https://fwd.innopolis.university/api/comic?${paramString}`)
+        .then(response => response.json())
+        .then((response) => {
+            console.log(response);
+            document.getElementById("photo").src=response.img;
+            document.getElementById("photo").alt=response.alt;
+            document.getElementById("photo").title=response.safe_title;
+            const event = new Date(Date.UTC(response.year, response.month, response.day));
+            document.getElementById("date").textContent="Date of publishing: " + event.toLocaleDateString();
+        })
+   });
+}
+
+
+window.onload = getXKCD;
