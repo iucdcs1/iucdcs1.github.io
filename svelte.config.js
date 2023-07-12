db73d141a80ee9796e9d52202d95eaeabd59422e
iucdcs1 <130791096+iucdcs1@users.noreply.github.com>
2023-07-12 16:38:55 +0300

    Update svelte.config.js

--- svelte.config.js
+++ svelte.config.js
@@ -15,7 +15,7 @@ const config = {
 		// See https://kit.svelte.dev/docs/adapters for more information about adapters.
 		adapter: adapter(),
 		paths: {
-			base: dev ? '' : '/iucdcs1.github.io',
+			base: dev ? '' : '/iucdcs1.github.io'
 		  }
 	}
 };
